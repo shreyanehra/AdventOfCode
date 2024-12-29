13ea37196cbf9e130aff318fe72062c5e8e2672c
shreyanehra <f20200146@goa.bits-pilani.ac.in>
2024-12-29 23:56:19 +0530

    aoc solution2 as function and classes

--- 2024/day1/aoc1_solution.py
+++ 2024/day1/aoc1_solution.py
@@ -1,21 +1,34 @@
+#recommended to use with when opening files in python
 
+class Solution:
+    def __init__(self, file_path):
+       
+        self.file_path = file_path
 
-with open("D:\\ACADS\\AOC\\2024\\day1\\aoc1_input.txt") as input_file:
-    content = input_file.read()
-    sepContent = content.split("\n")
+    def process_file(self):
+        """
+        reads the input file, processes the data, and computes the sum of differences.
+        :return: The sum of differences between sorted lists
+        """
+        with open(self.file_path, 'r') as input_file:
+            content = input_file.read()
+            # Separate by new line character
+            sep_content = content.strip().split("\n")
+        
+        list1 = []
+        list2 = []
 
-list1 = []
-list2 = []
-for pair in sepContent:
-    sepPair = pair.split("   ")
-    list1.append(int(sepPair[0]))
-    list2.append(int(sepPair[1]))
+        for pair in sep_content:
+            # Split using 3 spaces and convert to integers
+            sep_pair = pair.split("   ")
+            list1.append(int(sep_pair[0]))
+            list2.append(int(sep_pair[1]))
 
-list1_sorted = sorted(list1)
-list2_sorted = sorted(list2)
-sum_of_differences = 0
-print(len(list1))
-for i in range(len(list1)):
-    sum_of_differences += abs(list2_sorted[i]-list1_sorted[i])
+       
+        list1_sorted = sorted(list1)
+        list2_sorted = sorted(list2)
+        sum_of_differences = 0
+        for i in range(len(list1)):
+            sum_of_differences += abs(list2_sorted[i]-list1_sorted[i])
 
-print(sum_of_differences)    
+        return list1_sorted, list2_sorted, sum_of_differences
---
+++ 2024/day1/aoc_solution2.py
@@ -0,0 +1,44 @@
+#writing this as a function in solution class
+
+from aoc1_solution import Solution
+class Solution2:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+
+    def similarity_score(self):
+
+        '''
+        param file1 : string containing input file location
+        return : similarity_score
+        '''
+        sol1 = Solution(self.file_path)
+        result1 = sol1.process_file()
+        list1_sorted = result1[0]
+        list2_sorted = result1[1]
+        counter = 0
+        i=0
+        j=0
+        similarScore = 0
+
+        for i in range(len(list1_sorted)):
+            while (list1_sorted[i]>list2_sorted[j]):
+                j+=1
+            if (list1_sorted[i]==list2_sorted[j]):
+                while (list1_sorted[i]==list2_sorted[j]):
+                    counter+=1
+                    j+=1
+                similarScore += list1_sorted[i]*counter    
+            counter = 0   
+        
+        return similarScore
+    
+
+
+if __name__ == "__main__":
+    solution2 = Solution2("D:\\ACADS\\AOC\\2024\\day1\\aoc1_input.txt")           
+    
+    print(solution2.similarity_score())
+
+        
+
